4d7ad910cc1097e5e9b4ebf2470ed3c86112ea45
suyeshb@gmail.com <suyeshb@gmail.com>
2013-08-15 17:45:59 -0400

    suddenly index action broke

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'e8ae3087235d4c099d370e6aa67d170f1f013f8150bfb3d41e2e2dc213a7d99d5ad8e26c346e8909204f1f13ba128231fa870959ce5af10afe185b44f1ebf11c'
